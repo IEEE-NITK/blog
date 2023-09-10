28cec42fd8eec212a43e6ab29a9cad6cf03cd9be
Vignaraj-pai <vignarajpai@outlook.com>
2023-09-10 15:59:51 +0530

    Fixed script - works for single author + 2 author

--- scripts/uploadBlog.py
+++ scripts/uploadBlog.py
@@ -38,7 +38,7 @@ def writeUserInfo(githubUsername):
     email = input('Enter ' + githubUsername + ' email: ')
     author_image = input('Enter '+ githubUsername + ' image name (This is the name of the profile picture of the image of the author with extension THAT MUST BE IN THE images/ folder in THIS CURRENT DIRECTORY) : ')
     imageName = YEAR + name.replace(' ', '-') + '.jpg'
-    with open('author_info.txt', 'a', encoding='utf-8') as f:
+    with open('author_info.txt', 'w', encoding='utf-8') as f:
         f.write(githubUsername + ':\n')
         f.write("  name: " + name + '\n')
         f.write("  github: " + githubUsername + '\n')
@@ -64,6 +64,10 @@ def parse(markdownBlog):
         for (i, line) in enumerate(lines):
             if line.startswith('title:'):
                 blogTitle = line.split(':')[1].strip()
+                blogTitle = blogTitle.replace('"', '')
+                blogTitle = blogTitle.replace("'", '')
+                blogTitle = blogTitle.replace("”", '')
+                blogTitle = blogTitle.replace("“", '')
             if line.startswith('date:'):
                 blogDate = line.split(':')[1].strip()
                 blogDate = blogDate.split(' ')[0]
@@ -104,7 +108,8 @@ def parse(markdownBlog):
     githubUsername2[0] = githubUsername2[0].replace("'", '')
 
     lines[githubUsername1[1]] = 'github_username: ' + githubUsername1[0] + '\n'
-    lines[githubUsername2[1]] = 'github_username_2: ' + githubUsername2[0] + '\n'
+    if (githubUsername2[0] != '' and githubUsername2[1] != 0):
+        lines[githubUsername2[1]] = 'github_username_2: ' + githubUsername2[0] + '\n'
 
     if githubUsername2[0] != '':
         print('2 Author Blog')
@@ -117,9 +122,9 @@ def parse(markdownBlog):
         print('Author 1: ' + githubUsername1[0])
         writeUserInfo(githubUsername1[0])
 
-    
     # Write the lines back to the file 
-
+    if blogFileName == '':
+        blogFileName = blogDate + '-' + blogTitle.replace(' ', '-') + '.md'
     open(blogFileName, 'w', encoding='utf-8').writelines(lines)
     
     finalSteps(blogFileName, markdownBlog)
